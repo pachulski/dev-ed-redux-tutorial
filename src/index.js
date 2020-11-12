ce9ebf771343c9aa7409dc22abf9a553aa7ace9b
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-11-12 23:59:37 +0100

    moved reducers to separate files

--- src/index.js
+++ src/index.js
@@ -3,39 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-// ACTION - Describes what you want to do. Function that returns an object with an action name (type)
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  };
-};
+// // ACTION - Describes what you want to do. Function that returns an object with an action name (type)
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT',
+//   };
+// };
 
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  };
-};
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT',
+//   };
+// };
 
-// REDUCER - Describes how your actions transform the state into the next state. It checks which action you did and based on that it modifies the state. Function with actual action
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-};
+// // REDUCER - Describes how your actions transform the state into the next state. It checks which action you did and based on that it modifies the state. Function with actual action
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//   }
+// };
 
-// STORE -> GLOBALIZED STATE
-const store = createStore(counter);
+// // STORE -> GLOBALIZED STATE
+// const store = createStore(counter);
 
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
+// // Display it in the console
+// store.subscribe(() => console.log(store.getState()));
 
-// DISPATCH - This is where we execute the action
-store.dispatch(increment());
+// // DISPATCH - This is where we execute the action
+// store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
